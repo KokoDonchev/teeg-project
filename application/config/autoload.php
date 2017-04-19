0f1c2d01c4de2574d2e1226ac1e105c2b3b7b6c0
Nikola Donchev <ndon4ev@gmail.com>
2017-04-19 21:04:27 +0800

    changed name

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('data', 'search');
+$autoload['model'] = array('data', 'lookup');
--- application/models/lookup.php
+++ application/models/lookup.php
@@ -1,6 +1,6 @@
 <?php
 if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Search extends CI_Model {
+class Lookup extends CI_Model {
 
 }
